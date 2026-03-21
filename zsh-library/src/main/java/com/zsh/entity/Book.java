fc5b93217fdab714f77e507ce9668902c69df3ed
Zsh-cs <1294012402@qq.com>
2026-03-22 01:35:34 +0800

    修复了Lombok

--- zsh-library/src/main/java/com/zsh/entity/Book.java
+++ zsh-library/src/main/java/com/zsh/entity/Book.java
@@ -3,9 +3,15 @@ package com.zsh.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @TableId(type = IdType.AUTO)
@@ -20,81 +26,4 @@ public class Book {
     private LocalDate publishDate;
     private Double price;
 
-    public Book() {
-    }
-
-    public Book(Long id, String title, String author, String isbn, String description, String publisher, LocalDate publishDate, Double price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.description = description;
-        this.publisher = publisher;
-        this.publishDate = publishDate;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public LocalDate getPublishDate() {
-        return publishDate;
-    }
-
-    public void setPublishDate(LocalDate publishDate) {
-        this.publishDate = publishDate;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
--- zsh-library/src/main/java/com/zsh/result/Result.java
+++ zsh-library/src/main/java/com/zsh/result/Result.java
@@ -1,46 +1,16 @@
 package com.zsh.result;
 
+import lombok.Data;
+
 /**
  * 后端统一返回结果
  */
+@Data
 public class Result<T> {
 
     private Integer code;
     private T data;
     private String msg;
-
-    public Result() {
-    }
-
-    public Result(Integer code, T data, String msg) {
-        this.code = code;
-        this.data = data;
-        this.msg = msg;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
 
     public static <T> Result<T> success() {
         Result<T> result = new Result<T>();
