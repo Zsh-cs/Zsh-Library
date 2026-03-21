b766fe02be711cdd4b30b4dec61074f0b3608e2c
Zsh-cs <1294012402@qq.com>
2026-03-21 20:34:45 +0800

    搭建了后端初始工程，使用SpringBoot 3.5.12

---
+++ zsh-library/src/main/java/com/zsh/ZshLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.zsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZshLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZshLibraryApplication.class, args);
+    }
+
+}
